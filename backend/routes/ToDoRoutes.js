3cb8e4c3732f98710a4949c5c60adc07e9af3868
Milad Joodi <winbeta.com@gmail.com>
2024-08-05 23:29:27 +0330

    Update ToDoRoutes.js

--- backend/routes/ToDoRoutes.js
+++ backend/routes/ToDoRoutes.js
@@ -1,1 +1,9 @@
 const { Router } = require("express");
+const { getToDos, saveToDo } = require("../controllers/ToDoController");
+
+const router = Router();
+
+router.post("/get", getToDos);
+router.post("/save", saveToDo)
+
+module.exports = router;
