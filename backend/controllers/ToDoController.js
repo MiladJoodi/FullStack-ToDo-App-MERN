c609bfe3cd66f73e999d8a96bc8f58b63f506346
Milad Joodi <winbeta.com@gmail.com>
2024-08-05 23:26:34 +0330

    up

--- backend/controllers/ToDoController.js
+++ backend/controllers/ToDoController.js
@@ -15,14 +15,14 @@ module.exports.getToDos = getToDos
 // Save
 const saveToDo = async (req, res) => {
     try {
-        const {toDo, email} = req.body
-        ToDoModel.create({toDo, email}).then((data)=>{
+        const { toDo, email } = req.body
+        ToDoModel.create({ toDo, email }).then((data) => {
             console.log("Saved...");
             res.status(201).send(data);
         })
-    } catch(error) {
+    } catch (error) {
         console.log(error)
-        res.status(400).json({ msg: "Error" })
+        res.send({ error: error, msg: "Something went wrong..." }).json({ msg: "Error" })
     }
 }
 module.exports.saveToDo = saveToDo
