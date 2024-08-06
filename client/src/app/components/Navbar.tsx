63265a43da4079d5df280ed658a13b32c07d1eae
Milad Joodi <winbeta.com@gmail.com>
2024-08-06 18:33:00 +0330

    up

---
+++ client/src/app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className="flex justify-between items-center py-4 text-white">
+            Navbar
+        </div>
+    );
+}
+
+export default Navbar;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="max-w-[400px] mx-auto">
-
+      <Navbar />
     </main>
   );
 }
