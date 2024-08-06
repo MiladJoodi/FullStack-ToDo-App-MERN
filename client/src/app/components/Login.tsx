49b6423c83530b328b8e25f155ee351f1ae50b01
Milad Joodi <winbeta.com@gmail.com>
2024-08-06 18:49:29 +0330

    Update Login.tsx

--- client/src/app/components/Login.tsx
+++ client/src/app/components/Login.tsx
@@ -1,13 +1,16 @@
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 
 
 const Login = () => {
     return (
         <div className="min-h-screen grid place-items-center">
-            <button className="bg-white px-8 py-4 flex gap-2 items-center">
-                <FcGoogle />
+            <button className="bg-white px-8 py-4 flex gap-2 items-center"
+             onClick={()=> signIn("google")}
+             >
+                <FcGoogle size={30} />
                 Sign In with Google
-                </button>
+            </button>
         </div>
     );
 }
